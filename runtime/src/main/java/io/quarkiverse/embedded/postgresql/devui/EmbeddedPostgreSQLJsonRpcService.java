0680696c03020fc3db228c2ae221623702658c64
Dmitrii Tikhomirov <chani.liet@gmail.com>
2025-06-23 21:07:58 -0700

    rollback

--- runtime/src/main/java/io/quarkiverse/embedded/postgresql/devui/EmbeddedPostgreSQLJsonRpcService.java
+++ runtime/src/main/java/io/quarkiverse/embedded/postgresql/devui/EmbeddedPostgreSQLJsonRpcService.java
@@ -1,16 +1,17 @@
 package io.quarkiverse.embedded.postgresql.devui;
 
-import static io.quarkus.runtime.LaunchMode.DEVELOPMENT;
-
+import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import jakarta.inject.Inject;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
+import io.quarkus.devui.runtime.config.ConfigDescription;
 import io.quarkus.devui.runtime.config.ConfigDescriptionBean;
 import io.quarkus.runtime.LaunchMode;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import static io.quarkus.runtime.LaunchMode.DEVELOPMENT;
 
 public class EmbeddedPostgreSQLJsonRpcService {
 
@@ -21,11 +22,14 @@ public class EmbeddedPostgreSQLJsonRpcService {
     String jdbcUrl;
 
     public int getDatasourcePort() {
-        final String port = LaunchMode.current().equals(DEVELOPMENT)
-                ? jdbcUrl
-                : configDescriptionBean.getAllConfig().stream()
-                        .filter(c -> c.getName().equalsIgnoreCase("quarkus.datasource.jdbc.url")).findFirst().get()
-                        .getConfigValue().getValue();
+        String port;
+        if (LaunchMode.current().equals(DEVELOPMENT)) {
+            port = jdbcUrl;
+        } else {
+            Optional<ConfigDescription> config = configDescriptionBean.getAllConfig().stream()
+                    .filter(c -> c.getName().equalsIgnoreCase("quarkus.datasource.jdbc.url")).findFirst();
+            port = config.get().getConfigValue().getValue();
+        }
         // Define a regex pattern to match numbers
         Pattern pattern = Pattern.compile("\\d+");
 
